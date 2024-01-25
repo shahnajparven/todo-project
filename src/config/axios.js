2ea623e2c3511cc6e148150548e54e6ca22170f3
shahnaj parven <shahnajritu21@gmail.com>
2024-01-25 19:33:14 +0600

    change

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiInstance = axios.create({
     // baseURL: process.env.VITE_API_BASE_URL,
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.URL,
     timeout: 1000,
     headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     withCredentials: true,
