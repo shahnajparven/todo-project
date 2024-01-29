e6699c669f6e922c8e65489fb5778e0f319753d2
shahnaj parven <shahnajritu21@gmail.com>
2024-01-29 18:31:12 +0600

    change

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,48 @@
+// "use client";
+
+// import apiInstance from "@/config/axios";
+// import { Box } from "@mui/material";
+// import axios from "axios";
+// import React, { useEffect } from "react";
+
+// const About = () => {
+//   useEffect(() => {
+//     const getSessionData = async () => {
+//       try {
+//         // const config = {
+//         //   headers: {
+//         //     "Content-Type": "application/json",
+//         //   },
+//         // };
+
+//         const data = await apiInstance.get("/api/mytask");
+
+//         // console.log(res?.data, "data");
+//         // const datas = res?.data;
+//         return data;
+//       } catch (error) {
+//         console.error("Error fetching session data:", error);
+//       }
+//     };
+
+//     getSessionData();
+//   }, []);
+
+//   const tasks = getSessionData();
+ 
+//   return (
+//     <Box>
+//       <h2>hnhbh</h2>
+//       {tasks?.map((item, id) => (
+//         <Box>
+//           <h2>{item.title}</h2>
+//         </Box>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default About;
 import React from 'react'
 
 const About = () => {
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-    // baseURL: process.env.VITE_API_BASE_URL,
     baseURL: process.env.URL,
     timeout: 1000,
     headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
